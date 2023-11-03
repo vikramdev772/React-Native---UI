44c29a81b0724be7b35c3a892560f1ff79228524
vikram <vikramdev772@gmail.com>
2023-11-03 11:50:53 +0530

    UI

---
+++ d/constants/colors.js
@@ -0,0 +1,9 @@
+const COLORS = {
+    white: "#FFFFFF",
+    black: "#222222",
+    primary: " rgb(21, 77, 218)", //rgb(0, 128, 255)",
+    secondary: "rgb(169, 67, 232)",
+    grey: "#CCCCCC"
+}
+
+export default COLORS;
